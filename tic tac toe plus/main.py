4e11b9968e543b071e3569dcbe4c611079520869
Griffin Leonard <47362102+griffin-leonard@users.noreply.github.com>
2022-07-10 21:28:30 -0700

    Add files via upload

--- tic tac toe plus/main.py
+++ tic tac toe plus/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #
 # Author: Griffin Leonard
-# Title: colors game
-# Genres: metroidvainia, 2d platformer
+# Title: tic tac toe plus
 
 # LOAD MODULES
 from matplotlib.pyplot import grid
